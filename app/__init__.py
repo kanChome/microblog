33173b0d677ecbf635ef2c35fa4925ae335cb470
kanChome <hellbird1998@gmail.com>
2026-01-04 00:59:05 +0900

    add: エラー通知をファイルで行うように

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from logging.handlers import SMTPHandler
+from logging.handlers import RotatingFileHandler, SMTPHandler
+from datetime import datetime, timezone
 import logging
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -28,5 +30,15 @@ if not app.debug:
       credentials=auth, secure=secure)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
+
+  if not os.path.exists('logs'):
+    os.mkdir('logs')
+  file_handler = RotatingFileHandler('logs/' + datetime.now(timezone.utc).strftime('%Y-%m-%d') + '-microblog.log', maxBytes=10240, backupCount=10)
+  file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+  file_handler.setLevel(logging.INFO)
+  app.logger.addHandler(file_handler)
+  
+  app.logger.setLevel(logging.INFO)
+  app.logger.info('Microblog startup')
 
 from app import routes, models, errors
